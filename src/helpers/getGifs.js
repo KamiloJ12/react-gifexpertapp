a4466804d1f298c63541eda2bce37bdaaf7387e1
KamiloJ12 <camilo12052000@hotmail.com>
2022-04-18 22:33:55 -0500

    docs actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async ( category ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=20&api_key=7lN5ubz0G3FO13uMmuww5jJKYtoW0B8b`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=20&api_key=7lN5ubz0G3FO13uMmuww5jJKYtoW0B8b`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
